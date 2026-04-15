08f752cbde33cf2849cae6e8dbc7ecf1f84b3bf9
roymutua <roy.ngure@gmail.com>
2026-04-15 13:05:03 +0300

    cs50

--- cs50/1Function_Variables.py
+++ cs50/1Function_Variables.py
@@ -20,3 +20,21 @@ c = round(x / y, 2) #rounded to 2 decimal places
 
 print(f"{z:,}") #used to input (,) after 3 digit (eg. 1,000)
 print(f"{c:.2f}") #F string approach to specify how many decimal places
+
+def main ():
+    names = input("What is your name? ")
+    hello(names)
+
+def hello(to='world'):
+        print(f"Hello, {to}!")
+
+main()
+
+def main ():
+    k = float(input("What is k? "))
+    print("x squared is", square(k))
+
+def square(n):
+    return n ** n
+
+main()
